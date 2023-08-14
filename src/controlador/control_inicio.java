88e512ba5c6f35366616eea47c50fea07fc9743f
Renato <devrenatonavarro@gmail.com>
2023-08-14 07:15:11 -0500

    proyecto finalizado

---
+++ src/controlador/control_inicio.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import vista.*;
+
+/**
+ *
+ * @author Anahi
+ */
+public class control_inicio {
+    public static void main(String[] args) {
+        
+        vista_presentacion vista=new vista_presentacion();
+        vista.setTitle("SoftMathEdu");
+        vista.setLocationRelativeTo(null);
+        vista.setVisible(true);
+    }
+    
+}
